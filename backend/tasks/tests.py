6e2f191eda2fe6ef240545627526aae1473a18a4
PasupathiG03 <pasupathi0322@gmail.com>
2025-10-15 12:35:11 +0530

    modified test

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -3,3 +3,11 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
+
+        
+
+
+
+
+def test_fail():
+    assert 1 == 2
