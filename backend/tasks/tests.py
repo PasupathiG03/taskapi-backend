c80f23fb3bd1146e78da700c2c0d1bca3ed615aa
PasupathiG03 <pasupathi0322@gmail.com>
2025-10-15 14:44:07 +0530

    dev

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -4,6 +4,6 @@ class SimpleTest(TestCase):
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
 
-class FailTest(TestCase):
-    def test_fail(self):
-        self.assertEqual(1, 2)  # This test will fail intentionally
+# class FailTest(TestCase):
+#     def test_fail(self):
+#         self.assertEqual(1, 2)  # This test will fail intentionally
